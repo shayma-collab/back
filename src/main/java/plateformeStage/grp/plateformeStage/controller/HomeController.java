a5671a5b0123c301f4886b8f2dd7a298a2512258
jbuguys <wassimnefzi120@gmail.com>
2025-08-13 15:58:25 +0100

    cleaned imports

--- src/main/java/plateformeStage/grp/plateformeStage/controller/HomeController.java
+++ src/main/java/plateformeStage/grp/plateformeStage/controller/HomeController.java
@@ -1,24 +1,7 @@
 package plateformeStage.grp.plateformeStage.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import plateformeStage.grp.plateformeStage.entity.DemandeStage;
-import plateformeStage.grp.plateformeStage.entity.Etudiant;
-import plateformeStage.grp.plateformeStage.entity.OffreStage;
-import plateformeStage.grp.plateformeStage.repository.DemandeStageRepository;
-import plateformeStage.grp.plateformeStage.repository.EtudiantRepository;
-import plateformeStage.grp.plateformeStage.repository.OffreStageRepository;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
--- src/main/java/plateformeStage/grp/plateformeStage/dto/DemandeStageRequest.java
+++ src/main/java/plateformeStage/grp/plateformeStage/dto/DemandeStageRequest.java
@@ -2,8 +2,6 @@ package plateformeStage.grp.plateformeStage.dto;
 
 import java.time.LocalDate;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class DemandeStageRequest {
